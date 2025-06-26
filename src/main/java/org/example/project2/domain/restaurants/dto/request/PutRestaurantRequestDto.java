cf8978cb7f0fcce392bbebe49ca8f7d8359433cf
박주성 <121250776+Parkjuseong319@users.noreply.github.com>
2025-06-26 21:28:52 +0900

    feat: 더미데이터 및 ResponseDTO 값 추가

--- src/main/java/org/example/project2/domain/restaurants/dto/request/PutRestaurantRequestDto.java
+++ src/main/java/org/example/project2/domain/restaurants/dto/request/PutRestaurantRequestDto.java
@@ -1,9 +1,6 @@
 package org.example.project2.domain.restaurants.dto.request;
 
-import jakarta.validation.constraints.NotNull;
-
 public record PutRestaurantRequestDto(
-        Long id,
         String name,
         String address,
         String description,
